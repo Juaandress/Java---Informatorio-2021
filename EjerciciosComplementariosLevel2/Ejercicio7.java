fc77c6f1863732ab7034006c8ce0de9f4d4d48ac
Juan Andrés <67118830+Juaandress@users.noreply.github.com>
2021-06-07 23:54:56 -0300

    Actualizo el ejercicio 7

--- EjerciciosComplementariosLevel2/Ejercicio7.java
+++ EjerciciosComplementariosLevel2/Ejercicio7.java
@@ -7,6 +7,23 @@ public class Ejercicio7 {
 //    debe frenar (no se incluye en el cálculo)
 //    Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4
     public static void main(String[] args) {
-
+        
+        int a = 1;
+        int b =  6;
+        fizzBuzzFuncion(a,b);
+        System.out.print(fizzBuzzFuncion(a, b));
+    }
+        public static List fizzBuzzFuncion(int a, int b) {
+            List<String> numeros = new ArrayList<>();
+            for (int i = a; i < b; i++) {
+                if ((i%2==0) && (i%3==0)) {numeros.add("FizzBuzz");}
+                else if (i%2==0) { numeros.add("Fizz");}
+                else if (i%3==0) {numeros.add("Buzz");}
+                else {String num = String.valueOf(i);
+                    numeros.add(num);}               
+            }
+            return numeros;
+            
+        }
     }
 }
